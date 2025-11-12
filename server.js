be10bfbaac7dea5e89aebd8cd1b6cf78f7f5aa51
reck74 <reck@airp.ws>
2025-11-12 12:01:39 -0500

    Commit:Corregida apikey quemada

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// TODO: Mover esto a variables de entorno antes de ir a producción - no olvidar!
-const API_KEY = "dj-walc-2023-super-secreto-y-muy-largo-para-ser-encontrado";
+// la api key ya esta en las variables de entorno
 
 
 app.use(express.static(path.join(__dirname, 'public')));
